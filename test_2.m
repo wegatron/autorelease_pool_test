293205fbeb6796bd429f7e8e8139930a2e3b7683
shengwei.ZHANG <shengwei.ZHANG@quvideo.com>
2023-05-06 11:18:45 +0800

    update test

--- test_2.m
+++ test_2.m
@@ -39,6 +39,7 @@ int main(int argc, char * argv[])
     __weak NSString *weak_str_autorelease;
     __weak Student *weak_stu_release;
     __weak Student *weak_stu_autorelease;
+    Student * strong_stu_release;
 
     
     int i = 0;
@@ -49,8 +50,9 @@ int main(int argc, char * argv[])
         NSString *heap_str_release = [[NSString alloc] initWithFormat:@"堆区string-release"];
         NSString *heap_str_autorelease = [NSString stringWithFormat:@"堆区string-autorelease"];
 
-        Student * heap_stu_release = [[Student alloc] initWithName: @"张三-student-release" id: i];
-        Student * heap_stu_autorelease = createStudent(@"李四-student-auto-release",  i);
+        Student * heap_stu_release = [[Student alloc] initWithName: @"张三-student-release" id: i];        
+        Student * heap_stu_autorelease = createStudent(@"李四-student-auto-release",  i+1);
+        Student * heap_stu_release_2 = [[Student alloc] initWithName: @"王五-student-release" id: i+2];
 
         printf("const_str size: %lu\n", malloc_size((__bridge const void *)const_str));
         printf("stack_str size: %lu\n", malloc_size((__bridge const void *)stack_str));
@@ -62,6 +64,9 @@ int main(int argc, char * argv[])
 
         weak_str_release = heap_str_release;
         weak_str_autorelease = heap_str_autorelease;
+        weak_stu_release = heap_stu_release;
+        weak_stu_autorelease = heap_stu_autorelease;
+        strong_stu_release = heap_stu_release_2;
         _objc_autoreleasePoolPrint();
     }
 
@@ -69,6 +74,7 @@ int main(int argc, char * argv[])
     NSLog(@"weak_str_autorelease: %@\n", weak_str_autorelease);
     NSLog(@"weak_stu_release: %p\n", weak_stu_release);
     NSLog(@"weak_stu_autorelease: %p\n", weak_stu_autorelease);
+    NSLog(@"strong_stu_release: %p\n", strong_stu_release);
     _objc_autoreleasePoolPrint();
     printf("finish!!!\n");
     return 0;
